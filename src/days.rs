5eddaafb9d4629bf6208d9392311d4aa8411e9d8
Tim Fratangelo <tfrat84@gmail.com>
2024-12-13 18:23:45 -0500

    Simplified execution

--- src/days.rs
+++ src/days.rs
@@ -1,5 +1,3 @@
-use crate::Part;
-
 mod five;
 mod four;
 mod one;
@@ -12,14 +10,14 @@ pub trait Day {
     fn part_two(&self, input: &str) -> String;
 }
 
-pub fn get_day(day: &u8, part: &Part) -> Result<Box<dyn Day>, String> {
-    match (day, part) {
-        (1, _) => Ok(Box::new(one::DayOne::default())),
-        (2, _) => Ok(Box::new(two::DayTwo::default())),
-        (3, _) => Ok(Box::new(three::DayThree::default())),
-        (4, _) => Ok(Box::new(four::DayFour::default())),
-        (5, _) => Ok(Box::new(five::DayFive::default())),
-        (6, _) => Ok(Box::new(six::DaySix::default())),
+pub fn get_day(day: &u8) -> Result<Box<dyn Day>, String> {
+    match day {
+        1 => Ok(Box::new(one::DayOne::default())),
+        2 => Ok(Box::new(two::DayTwo::default())),
+        3 => Ok(Box::new(three::DayThree::default())),
+        4 => Ok(Box::new(four::DayFour::default())),
+        5 => Ok(Box::new(five::DayFive::default())),
+        6 => Ok(Box::new(six::DaySix::default())),
         _ => Err(format!("Day {} not supported.", day)),
     }
 }
